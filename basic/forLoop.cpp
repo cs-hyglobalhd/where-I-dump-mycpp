310535c02c8868a5fdd58a856d859ebb9682d3ec
HyGlobalHD <GamerXzavier@gmail.com>
2019-10-14 15:52:22 +0800

    Update forLoop.cpp
    completed the program code, add commentary, more specific readable commentary.

--- basic/forLoop.cpp
+++ basic/forLoop.cpp
@@ -4,13 +4,21 @@
 using namespace std;
 
 int main() {
-  int num;
+  int num; // declare variable num with integer data type
   
-  cout << "Please enter an integer: ";
-  cin >> num;
+  cout << "Please enter an integer: "; // print sentences to screen
+  cin >> num; // assign user input to num variable
   
-  for(int i = 0; i < num; i++) {
-    // TODO
+  for(int i = 0; i < num; i++) { // for loop
+    // explanation: declare cariable i with integer datatype
+    // assign value 0 to variable num
+    // using LOGIC parameters, check if variable i is less than variable num
+    // if true, continue next statement of i++
+    // i++, means i = i + 1;
+    
+    cout << i << endl; // print the variable i, until the loop finished //LOGIC is false
   }
+  
+  return 0; // indicated that the program end successfull
   
 }
