51a2b92367978d0087189ecf49ba596e958b2edb
HyGlobalHD <GamerXzavier@gmail.com>
2019-10-14 11:06:41 +0800

    Create forLoop.cpp
    wait

---
+++ basic/forLoop.cpp
@@ -0,0 +1,16 @@
+// An example of program that use for-Loop
+// Author: HyGlobalHD <https://github.com/hyglobalhd>
+#include<iostream>
+using namespace std;
+
+int main() {
+  int num;
+  
+  cout << "Please enter an integer: ";
+  cin >> num;
+  
+  for(int i = 0; i < num; i++) {
+    // TODO
+  }
+  
+}
