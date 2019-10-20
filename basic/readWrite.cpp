130b69443593928dd799b1ef1932c66a5b883eb2
HyGlobalHD <GamerXzavier@gmail.com>
2019-10-20 11:02:33 +0800

    commentary

--- basic/readWrite.cpp
+++ basic/readWrite.cpp
@@ -2,15 +2,17 @@
 // Author: HyGlobalHD <https://github.com/hyglobalhd>
 #include <iostream>
 #include <string>
-#include <fstream>
+#include <fstream> // this library must be included to read,write,create file
 using namespace std;
 
 void readWrite(string str); // new function must be declare first here
 
 int main() {
-    cout<<"File Reading/Writing Program: \n";
+    cout<<"\t\t\tFile Reading/Writing Program: \n";
+    
     readWrite("\nFirst attempt!\n");
     readWrite("\nSecond attempt!\n");
+    readWrite("\Third attempt!\n");
 
     cout<<"\nPress <enter> to exit console: "; // prompt to get the user to press enter
     cin.get(); // continue if true ( this is to get the key enter )
@@ -21,14 +23,14 @@ void readWrite(string str) {
     cout<<str;
     fstream file;
     file.open("file.txt", ios::out|ios::in);//Attempt to open file.
-    if(file.is_open()) {
+    if(file.is_open()) { // check if the file is available and can be opened
         cout<<"Reading...\n";
-        string temp;
+        string temp; // assign file's content to temp variable
         while(getline(file,temp) && cout<<temp<<'\n');//Print data in file!
-    }else {
+    }else { // if not, create the file, and add a content it it
         cout<<"Writing...\n";
-        file.open("file.txt", ios::out|ios::in|ios::trunc);//TRUNCATING to make new file!
-        file<<"Hello World!\nThis is a string!";
+        file.open("file.txt", ios::out|ios::in|ios::trunc); //TRUNCATING to make new file!
+        file<<"Hello World!\nThis is a string!\nThis is a third Line!!"; // Line that write the data
     }
     file.close();//File must be properly closed if opened! Else issues may arise.
 }
